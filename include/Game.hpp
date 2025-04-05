17c6635e2212a6c7b479475bdb967fd6e9f25f6a
chanmin0311 <ckstj1500@gmail.com>
2025-04-06 01:05:48 +0900

    feat: Add score display and start game on Enter HUD

--- include/Game.hpp
+++ include/Game.hpp
@@ -4,29 +4,45 @@
 
 #pragma once
 #include <SFML/Graphics.hpp>
-#include <vector>
 #include <memory>
-#include "Entity.hpp"
+#include <vector>
+
 #include "Bee.hpp"
 #include "Cloud.hpp"
+#include "Entity.hpp"
 
 class Game {
-    public:
+   public:
     Game();
     void run();
 
-    private:
+   private:
     void processInput();
     void update(sf::Time dt);
     void render();
 
+    // Variables to be rendered in the window
     sf::RenderWindow mWindow;
+
     sf::Texture mTextureBackground;
     sf::Sprite mSpriteBackground;
+
     sf::Texture mTextureTree;
     sf::Sprite mSpriteTree;
+
     sf::Clock mClock;
+
+    sf::Font mFont;
+    sf::Text mMessage;
+    sf::Text mScoreText;
+    int mScore;
+
+    // Entity vector
+    // This vector will hold all the entities in the game
     std::vector<std::unique_ptr<Entity>> mEntities;
+
+    // Game state variables
+    bool mPaused;
 };
 
 #endif  // GAME_HPP
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,12 +1,54 @@
 // Game.cpp
 #include "../include/Game.hpp"
 
-Game::Game() : mWindow(sf::VideoMode(1920, 1080), "Timber!!!", sf::Style::Fullscreen) {
-    mTextureBackground.loadFromFile("../assets/images/background.png");
+Game::Game()
+    : mWindow(sf::VideoMode(1920, 1080), "Timber!!!", sf::Style::Fullscreen),
+      mPaused(true),
+      mScore(0) {
+    // Find the font and load it
+    if (!mFont.loadFromFile("../assets/fonts/KOMIKAP_.ttf")) {
+        throw std::runtime_error("Failed to load font");
+    }
+    // Set up HUD (Heads Up Display)
+    // Set the font to our test
+    mMessage.setFont(mFont);
+    mScoreText.setFont(mFont);
+    // Assigin the actual text
+    mMessage.setString("Press Enter to start!");
+    mScoreText.setString("Score: 0");
+    // Set the size of the text
+    mMessage.setCharacterSize(75);
+    mScoreText.setCharacterSize(100);
+    // Set the color of the text
+    mMessage.setFillColor(sf::Color::White);
+    mScoreText.setFillColor(sf::Color::White);
+    // Position the text
+    // Get the bounding box of the text
+    sf::FloatRect textRect = mMessage.getLocalBounds();
+    // Set the origin of the text to the center
+    mMessage.setOrigin(textRect.left + textRect.width / 2.0f,
+                       textRect.top + textRect.height / 2.0f);
+    // Set the position of the text to the center of the screen
+    mMessage.setPosition(mWindow.getSize().x / 2.0f, mWindow.getSize().y / 2.0f);
+    // Set the position of the score text to the top left corner
+    mScoreText.setPosition(20, 20);
+
+    // Find the background texture and load it
+    // Set the background texture
+    if (!mTextureBackground.loadFromFile("../assets/images/background.png")) {
+        throw std::runtime_error("Failed to load background texture");
+    }
+    // Set the background sprite
     mSpriteBackground.setTexture(mTextureBackground);
 
-    mTextureTree.loadFromFile("../assets/images/tree.png");
+    // Find the tree texture and load it
+    // Set the tree texture
+    if (!mTextureTree.loadFromFile("../assets/images/tree.png")) {
+        throw std::runtime_error("Failed to load tree texture");
+    }
+    // Set the tree sprite
     mSpriteTree.setTexture(mTextureTree);
+    // Set the tree sprite position to cnter of the screen
     mSpriteTree.setPosition((mWindow.getSize().x / 2) - (mTextureTree.getSize().x / 2), 0);
 
     // Create Entities
@@ -29,11 +71,17 @@ void Game::processInput() {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
         mWindow.close();
     }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
+        mPaused = false;
+    }
 }
 
 void Game::update(sf::Time dt) {
-    for (auto& entity : mEntities) {
-        entity->update(dt);
+    if (mPaused == false) {
+        for (auto& entity : mEntities) {
+            entity->update(dt);
+        }
     }
 }
 
@@ -44,5 +92,12 @@ void Game::render() {
         entity->render(mWindow);
     }
     mWindow.draw(mSpriteTree);
+    // Draw the score
+    mScoreText.setString("Score: " + std::to_string(mScore));
+    mWindow.draw(mScoreText);
+    // Draw the message
+    if (mPaused) {
+        mWindow.draw(mMessage);
+    }
     mWindow.display();
 }
